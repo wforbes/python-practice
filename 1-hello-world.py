592f195dce9c8467eb45323cb4ab4e0e85b344b9
Will Forbes <wforbes@users.noreply.github.com>
2020-07-18 14:41:13 -0700

    hello world!
    The obligatory 'hello world' print function exploration!

---
+++ 1-hello-world.py
@@ -0,0 +1,11 @@
+# print function - one param
+print("Hello, World")  # output: Hello, World
+
+# print function - two params
+print("Hello", "World")  # output: Hello World
+
+# print function - params: two strings and a separator string
+print("Hello", "World", sep=", ")  # output: Hello, World
+
+# print function - params: two strings, separator string, and end string
+print("Hello", "World", sep=", ", end="!")  # output: Hello, World!
