cac1ac896a83d0ea19fd51dc7354365f5d15545a
Will Forbes <wforbes@users.noreply.github.com>
2020-07-22 22:50:04 -0700

    input function
    seems pretty basic... maybe ill find more stuff to do with input later

---
+++ 3-input.py
@@ -0,0 +1,4 @@
+# input function takes one parameter that's turns into a string
+#   it waits for the user to type something and returns that value
+name = input("What's your name?  ")
+print("You're awesome, " + name)
